8f2acb53c1d544ab95d5709edbbb17567ce417fe
Anna Boeri <annaboeri@gmail.com>
2018-04-07 14:57:00 -0700

    create user model and controller

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const 
+    mongoose = require('mongoose'),
+    bcrypt = require('bcrypt-nodejs'),
+    userSchema = new mongoose.Schema({
+        name: { type: String },
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true }
+    })
+
+userSchema.methods.generateHash = function(password) {
+    return bcrypt.compareSync(password, this.password) 
+}
+
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password)
+}
+
+userSchema.pre('save', function(next) {
+    if(this.isModified('password')) {
+        this.password = this.generateHash(this.password)
+    }
+    next()
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
