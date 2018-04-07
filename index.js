400b2dd55b2f37e892175fd90f203bb69b889f65
Anna Boeri <annaboeri@gmail.com>
2018-04-07 14:27:49 -0700

    basic file structure and server setup

---
+++ index.js
@@ -0,0 +1,17 @@
+const 
+    dotenv = require('dotenv').load()
+    express = require('express'),
+    app = express(),
+    logger = require ('morgan'),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose')
+    MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/project-4-api'
+    PORT = process.env.port || 3001
+
+mongoose.connect(MONGODB_URI, (err) => {
+    console.log(err || 'Connected to MongoDB')
+})
+
+app.listen(PORT, (err) => {
+    console.log(err || `Server running on port ${PORT}`)
+})
