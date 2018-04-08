e3558dfc6c693d614ac959d0b89dd223a354c009
Anna Boeri <annaboeri@gmail.com>
2018-04-07 17:34:27 -0700

    add react client app

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>My App</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
