7cc15804d9b2494796bc5a7cb22b3aeab0874d5d
Anna Boeri <annaboeri@gmail.com>
2018-04-10 09:53:45 -0700

    set up socket.io

--- client/src/views/Chat.jsx
+++ client/src/views/Chat.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
+import socketIOClient from 'socket.io-client'
 
-const Chat = (props) => {
-	return (
+class Chat extends React.Component {
+    state = {
+        endpoint: "http://localhost:3001",
+        color: 'white'
+    }
+
+    send = () => {
+        const socket = socketIOClient(this.state.endpoint)
+        socket.emit('change color', this.state.color)
+           
+    }
+
+    setColor = (color) => {
+        this.setState({ color })
+    }
+
+    render(){
+        const socket = socketIOClient(this.state.endpoint)
+        socket.on('change color', (col) => {
+            document.body.style.backgroundColor = col
+        })
+        return (
 		<div className='Chat'>
 			<h1>GLOBALink Chat</h1>
+            <div style={{ textAlign: "center" }}>
+                <button onClick={() => this.send()}>Change Color</button>
+
+                <button id="blue" onClick={() => this.setColor('blue')}>Blue</button>
+                <button id="pink" onClick={() => this.setColor('pink')}>Pink</button>
+            </div>
 		</div>
 	)
+ }
 }
 
 
--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const
     dotenv = require('dotenv').config(),
     express = require('express'),
     app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io')(server),
     logger = require ('morgan'),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose'),
+    Twitter = require('twitter'),
     MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/project-4-api'
     axios = require('axios'),
     httpClient = axios.create(),
@@ -12,6 +15,13 @@ const
     usersRoutes = require('./routes/users.js')
 
 const weatherApiKey = process.env.WEATHER_API_KEY
+
+// const twitterClient = new Twitter({
+//         consumer_key: process.env.TWITTER_CONSUMER_KEY,
+//         consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+//         access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+//         access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+//       });
 
 mongoose.connect(MONGODB_URI, (err) => {
     console.log(err || 'Connected to MongoDB')
@@ -31,6 +41,35 @@ app.get('/api/weather/:city', (req, res) => {
     })
 })
 
-app.listen(PORT, (err) => {
+// app.get('/api/twitter/:lat/:lng', (req, res) => {
+//     console.log(process.env.TWITTER_CONSUMER_KEY)
+//     twitterClient.get(`https://api.twitter.com/1.1/trends/closest.json?lat=${req.params.lat}&long=-${req.params.lat}`).then((apiResponse) => {
+
+//       })
+//       .catch(function (error) {
+//         throw error;
+//       }) 
+// })
+io.on('connection', socket => {
+    console.log("A new client has connected...")
+   // io.emit('new-user-connection', { boom: "BANANA!!" })
+
+    // socket.on('new-message', (message) => {
+    //     io.emit('broadcast-message', message)
+    // })
+
+    socket.on('change color', (color) => {
+        console.log('Color changed to: ', color)
+        io.sockets.emit('change color', color)
+    })
+    // disconnect is fired when a client leaves the server
+    socket.on('disconnect', () => {
+        console.log('user disconnected')
+    })
+})
+
+server.listen(PORT, (err) => {
     console.log(err || `Server running on port ${PORT}`)
 })
+
+
