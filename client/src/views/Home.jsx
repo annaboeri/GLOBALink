d109f4b9508b7952a9e586a1277b9d9ff893f811
Anna Boeri <annaboeri@gmail.com>
2018-04-13 12:42:20 -0700

    MVP achieved

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Home.css'
 
 const Home = (props) => {
 	return (
 		<div className='Home'>
-			<h2>Explore the world from your computer!</h2>
+			<h1>Explore the world from your computer!</h1>
+			<h2>Learn about cities around the world while chatting in real time with other users around the globe!</h2>
+			<h3>Click <Link to="/signup">here</Link> to sign up and start exploring.</h3>
 		</div>
 	)
 }
