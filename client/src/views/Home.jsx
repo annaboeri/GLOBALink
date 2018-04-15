0835d70ef20b01e87f8d29b14a148a794940831d
Anna Boeri <annaboeri@gmail.com>
2018-04-15 09:38:43 -0700

    update css

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -7,7 +7,7 @@ const Home = (props) => {
 		<div className='Home'>
 			<div className="text">
 				<h1>Explore the world from your computer!</h1>
-				<h2>Learn about cities around the world while chatting in real time with other users around the globe!</h2>
+				<h2>Learn about cities around the world while chatting in real time with other users from around the globe!</h2>
 				<h3>Click <Link to="/signup">here</Link> to sign up and start exploring.</h3>
 			</div>
 		</div>
