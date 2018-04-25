1c93518ffef2cdc3ff28e6f59b57ba8631cd5642
Anna Boeri <annaboeri@gmail.com>
2018-04-25 15:36:35 -0700

    create cities reducer and add to root reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
+import CitiesReducer from './reducer_cities'
 import RandomCity from "./reducer_random_city"
 
 
 const rootReducer = combineReducers({
+    cities: CitiesReducer,
     randomCity: RandomCity
   });
   
