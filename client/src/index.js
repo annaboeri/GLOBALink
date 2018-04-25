6f125356cf52f01d6b2d25576b334adeab785e51
Anna Boeri <annaboeri@gmail.com>
2018-04-25 15:21:38 -0700

    install react-redux and wrap app in provider in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 import 'milligram'
 import './styles.css'
 import App from './components/App'
+import reducers from './reducers'
 
 ReactDOM.render(
-	<Router><App id="App" /></Router>,
+	<Provider store={createStore(reducers)}>
+		<Router>
+			<App id="App" />
+		</Router>
+	</Provider>,
 	document.getElementById('root')
 )
