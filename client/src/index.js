2db61a7560454e8b1f6bacceb71db061c3b4ee82
Anna Boeri <annaboeri@gmail.com>
2018-04-30 10:03:39 -0700

    install and apply redux-promise middleware

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import ReduxPromise from 'redux-promise'
 import 'milligram'
 import './styles.css'
 import App from './containers/App'
 import reducers from './reducers'
 
-const store = createStore(reducers)
-console.log(store.getState())
+const createStoreWithMiddleware = applyMiddleware()(createStore)
+const store = createStoreWithMiddleware(reducers)
+console.log(store.getState(ReduxPromise))
 
 
 ReactDOM.render(
